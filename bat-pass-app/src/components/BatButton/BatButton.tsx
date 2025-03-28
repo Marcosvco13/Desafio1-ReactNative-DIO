648e0b9d689becedd19ecb90bc9da0708ed2f726
Marcos <84818139+Marcosvco13@users.noreply.github.com>
2025-03-28 10:49:37 -0300

    1.0.0
    Gerador de senhas apresentado como solicitado no desafio do curso DIO

---
+++ bat-pass-app/src/components/BatButton/BatButton.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import { Pressable, Text } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
+
+import { styles } from './BatButtonStyles';
+import { BatTextInput } from '../BatTextInput/BatTextInput';
+import generatePass from '../../services/passwordService';
+
+export function BatButton() {
+  const [pass, setPass] = useState('');
+
+  function handleGenerate(){
+    let generateToken = generatePass(); 
+    setPass(generateToken);
+  }
+
+  function handleCopyButton() {
+    Clipboard.setStringAsync(pass);
+    alert('Password copied to clipboard!');
+  }
+
+  return (
+    <>
+      <BatTextInput pass={pass}/>
+
+      <Pressable onPress={handleGenerate} style={styles.button}>
+          <Text style={styles.text}>Generate</Text>
+      </Pressable>
+
+      <Pressable onPress={handleCopyButton} style={styles.button}>
+          <Text style={styles.text}>⚡Copy</Text>
+      </Pressable>
+    </>
+  );
+}
